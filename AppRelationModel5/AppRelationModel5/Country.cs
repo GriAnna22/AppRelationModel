5f5227fdfb3c42a3c63150d9fd145329b5a1809f
GriAnna22 <92019410+GriAnna22@users.noreply.github.com>
2022-04-02 22:17:46 +0300

    Add files via upload

---
+++ AppRelationModel5/AppRelationModel5/Country.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppRelationModel5
+{
+    public class Country
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<Company> Companies { get; set; } = new List<Company>();
+    }
+}
